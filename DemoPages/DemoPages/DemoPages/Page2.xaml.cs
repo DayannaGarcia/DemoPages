b904c075acc3f37f4358d67fab8bc0782045bebc
DayannaGarcia <yayisgarcia15@gmail.com>
2018-08-30 17:12:35 -0500

    page3

--- DemoPages/DemoPages/DemoPages/Page2.xaml.cs
+++ DemoPages/DemoPages/DemoPages/Page2.xaml.cs
@@ -16,5 +16,28 @@ namespace DemoPages
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private async void btnTres_Clicked(object sender, EventArgs e)
+        {
+            this.lbOpcion.Text = string.Empty;
+            await Navigation.PushAsync(new Page3());
+
+        }
+
+        private async void btnTest_Clicked(object sender, EventArgs e)
+        {
+            this.lbOpcion.Text = string.Empty;
+            var resp = await DisplayAlert("Test","Esta seguro de relizar el test","Si","No");
+            if (resp)
+            {
+                await DisplayAlert("Inicio de Test","Super Bienvenido","Aceptar");
+                var opcion = await DisplayActionSheet("Opciones", "Cancelar", null, "Opcion 1", "Opcion 2", "Opcion 3");
+                lbOpcion.Text = opcion.ToString();
+            }
+            else
+            {
+                await DisplayAlert("Test Fallido", "En otra ocasion sera", "Aceptar");
+            }
+        }
+    }
 }
